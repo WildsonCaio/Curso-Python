162adedcccb1e22bef229952de18e28ea04e5fa4
João Gabriel Correa <joaogabrielcorrea1@hotmail.com>
2022-07-29 22:18:18 -0300

    novos exercicios

---
+++ condicional/1.py
@@ -0,0 +1,10 @@
+#https://wiki.python.org.br/EstruturaDeDecisao
+
+n1= int (input("Insira primeiro numero: "))
+n2= int (input("Insira segundo numero: "))
+if n1==n2:
+    print("São iguais")
+if n1>n2:
+    print(f"maior numero: {n1}")
+if n1<n2:
+    print(f"maior numero: {n2}")
---
+++ condicional/2.py
@@ -0,0 +1,8 @@
+n1= float (input("Insira primeiro numero: "))
+
+if n1== 0:
+    print("é neutro")
+elif n1>0:
+    print(f"é positivo {n1}")
+else:
+    print(f"é negativo {n1}")
