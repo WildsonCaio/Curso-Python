2d0ad732f907b5d0e89a80ed7bb9dc579a995071
João Gabriel Correa <joaogabrielcorrea1@hotmail.com>
2022-08-03 09:57:08 -0300

    Organização

--- python/senha e usuario.py
+++ python/senha e usuario.py
@@ -3,21 +3,12 @@ user = str ("Joao")
 nome = str (input("Insira seu usuario "))
 senha = str (input("Insira sua senha "))
 
-
-
-
-
-  
-
 if nome==user:
     print (" Usuario aceito insira senha")
 else:
     print (" Este usuario não encotrado ")
 
-
-
 if senha==cod:
     print (" acesso liberado")
-   
 else:
     print ("Senha incorreta")
