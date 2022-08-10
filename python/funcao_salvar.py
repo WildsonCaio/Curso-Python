7b241ae0badb2580a9cd3b4634d75198a0671cb6
João Gabriel Correa <joaogabrielcorrea1@hotmail.com>
2022-08-10 12:20:03 -0300

    Novos exercicios

--- python/funcao_salvar.py
+++ python/funcao_salvar.py
@@ -2,7 +2,9 @@
 nome = str (input("Crie seu usuario: "))
 senha = str (input("Crie sua senha: "))
 
+#abre ou criar com sobreescrever
 data=open('nome.txt','a')
+#escreve
 data.write(f"{nome}\n")
 
 data=open('senha.txt','a')
