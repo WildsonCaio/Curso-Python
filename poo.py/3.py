c94075188173561dfbe3b3b391957961d3238ae6
JOÃO GABRIEL CORREA <joao.correa@alunos.sc.senac.br>
2022-08-18 16:56:20 -0300

    Novos exercicios class e POO

--- poo.py/3.py
+++ poo.py/3.py
@@ -7,8 +7,10 @@ class Retangulo:
     def calcular_area(self):
             self.comprimento=float(input("Insira comprimento: "))
             self.largura=float(input("Insira largura: "))
-            return (f"A area é: {(self.largura*self.largura)} seu comprimento é {self.comprimento} e sua largura é {self.largura}")
+            area= self.largura*self.largura
+            return (f"A area é: {area} seu comprimento é {self.comprimento} e sua largura é {self.largura}")
             
 Retangulo_1=Retangulo('comprimento','largura')
 print (Retangulo_1.calcular_area())
-     
+
+
